233bbc80802c22c80a5debc36a0a0336e2724ef1
Nicola Muratori <nicola.muratori@gmail.com>
2021-08-07 17:52:52 +0200

    added copyright and license info

--- test/main/test_grownode.c
+++ test/main/test_grownode.c
@@ -1,11 +1,16 @@
-/* Example test application for testable component.
+// Copyright 2021 Nicola Muratori (nicola.muratori@gmail.com)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include <stdio.h>
 #include <string.h>
